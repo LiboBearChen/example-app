be5141684f7fc8befbdd3fe013fa1fdebd2fb4f1
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-03-23 18:03:05 -0400

    update

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,5 @@ use App\Http\Controllers\ProductsController;
 */
 
 Route::get('/products',[ProductsController::class,'index'] );
+Route::get('/products/about',[ProductsController::class,'about'] );
 
