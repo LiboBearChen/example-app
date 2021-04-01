9f31087392cd980af6f674bcc0ac6d018e76d014
LiboBearChen <50467944+LiboBearChen@users.noreply.github.com>
2021-03-31 21:56:47 -0400

    update

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -20,35 +20,10 @@
 </head>
 
 <body>
-    <header>
-        <!-- Navigation -->
-        <nav class="top-menu-container">
-            <div class="logo-header">
-                <a href="">
-                    <img 
-                    src="https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png" 
-                    alt="Logo personal portfolio"
-                    title="Logo personal portfolio"
-                    />
-                </a>
-            </div>
-
-            <ul>
-                <li>
-                    <a href="">About</a>
-                </li>
-                <li>
-                    <a href="">Portfolio</a>
-                </li>
-                <li>
-                    <a href="">Contact</a>
-                </li>
-            </ul>
-        </nav>
-    </header>
-
-    <!-- Hero background & content on top of hero background -->
-    <div id="hero-container">
+    @extends('layouts.app')
+    @section('content')
+<!-- Hero background & content on top of hero background -->
+<div id="hero-container">
         <div class="hero-wrapper">
             <h1>Multi-Disciplinary <br> <span class="line">Designer</span> <span class="orange-txt">Developer</span></h1>
 
@@ -187,18 +162,8 @@
             </p>
         </div>
     </div>
+    @endsection
+    
 
-    <!-- Footer -->
-    <footer>
-        <div class="container-footer">
-            <img 
-                src="https://cdn.pixabay.com/photo/2017/02/18/19/20/logo-2078018_960_720.png" 
-                alt="Logo personal portfolio"
-                title="Logo personal portfolio"
-                />
-            <p>
-                Personal Website
-            </p>
-    </footer>
 </body>
 </html>
